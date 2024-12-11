c9d534409da79e9463098a9f062a9a7e9c8189cc
2200090246 <148328823+2200090246@users.noreply.github.com>
2024-12-11 15:00:14 +0530

    Update CorsConfig.java

--- src/main/java/com/edu/erp/config/CorsConfig.java
+++ src/main/java/com/edu/erp/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("https://67595432c9a76900087d8a7a--eduerp.netlify.app") // Correct frontend URL
+                .allowedOrigins("https://jfsdrenderfront.onrender.com") // Correct frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials like cookies
